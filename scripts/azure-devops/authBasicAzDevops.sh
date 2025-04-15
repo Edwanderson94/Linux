5af601c0a3dad8882a80e1f634631b01776a4b06
Edwanderson Pereira <80295393+Edwanderson94@users.noreply.github.com>
2025-04-15 15:24:44 -0300

    Update authBasicAzDevops.sh

--- scripts/azure-devops/authBasicAzDevops.sh
+++ scripts/azure-devops/authBasicAzDevops.sh
@@ -3,23 +3,23 @@
 set -ex
 
 # --- 1.0 Configuração do Git ---
-git config --global user.email "service-azuredevops@mastercctvm.com.br"
+git config --global user.email "service@dominio.com.br"
 git config --global user.name "Azure DevOps"
 
 # --- 2.0 Definição de Variaveis.  ---
-ORG=$organizationName
+ORG=""
 ORG_URL="https://dev.azure.com/$ORG"
-BRANCHTARGET=$sourceBranchDefaultGitflowPRD  # Branch alvo do PR
-BRANCHOURCE=$sourceBranchDefaultDEV # Branch de origem do PR
-PAT=$autenticatePatSvc
+BRANCHTARGET=""  # Branch alvo do PR
+BRANCHOURCE="" # Branch de origem do PR
+PAT=""
 
 # --- 3.0 Autenticação ---
 echo "$PAT" | az devops login --organization "$ORG_URL"
 
 # --- 4.0 Remove espaços extras e codifica para URL (ex: "Master DevOps" → "Master%20DevOps") ---
-PROJECT=                        # Nome do projeto (pode ser utilizado com espaço)
+PROJECT=""                                   # Nome do projeto (pode ser utilizado com espaço)
 PROJECT=$(echo "$PROJECT" | xargs)           # Remove espaços no início/fim
-REPO=$repositoryName                         # Nome do repositório
+REPO=""                                      # Nome do repositório
 REPO_URL="https://$PAT@dev.azure.com/$ORG/$PROJECT/_git/$REPO"
 
 # --- 5.0 Setar o contexto do projeto ---
